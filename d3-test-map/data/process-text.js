c8b2b76ebca6a488ff106c4829112cab6604ab6f
katevdub <kate@Kates-MacBook-Pro.local>
2013-11-15 11:36:49 -0500

    Added text, moved coordinates to js file, added styles, adjusted size & center

--- d3-test-map/data/process-text.js
+++ d3-test-map/data/process-text.js
@@ -6,6 +6,29 @@ var process_text = [
 	{
 		headline: '2. USAID asks for NGO & shipping proposals - Washington, DC',
 		body: 'USAID requests proposals from aid agencies, like Catholic Relief Services, Save the Children or World Vision. The agencies propose how they would provide aid to the country, listing the specific food they’d like to send and how much.'
-	}
-	
+	},
+	{
+		headline: '3. Commodities are purchased - Kansas City',
+		body: 'Once the government approves an aid agency’s proposal, they sent the request for specific food items to the Kansas City Commodity Office, who sends out a call for bids to agribusiness. Agribusiness has two weeks to offer bids on how much they’d charge to provide specific products. Shipping companies also bid on how they’d transport the food.'
+	},
+	{
+		headline: '4. Commodities are gathered and sent on their way - Wisconsin',
+		body: 'The desired crops go from the farmers producing them to the mills where they’ll be processed and, in some cases, bagged (sometimes they’re shipped in bulk). Processing can include the addition of special vitamins and minerals to the product.'
+	},
+	{
+		headline: '5. Food arrives at its first stop - Chicago',
+		body: 'The crops travel by rail from the mill to “bridge facility,” a step toward their final U.S. destination, their departure port.'
+	},
+	{
+		headline: '6. Food arrives at U.S. departure port - Norfolk, Virginia',
+		body: 'This is where the food is loaded onto a big ship to begin it’s journey overseas. An inspector watches as its loaded onto the ship, making sure bags aren’t broken or otherwise sullied.'
+	},
+	{
+		headline: '7. Food arrives at destination port - Cotonou, Benin',
+		body: 'The foreign port is where the NGO takes possession of the aid. If it was shipped in bulk (like corn filling up the hull of the ship), it’s bagged here.'
+	},
+	{
+		headline: '8. Food gets to hungry people - Maradi, Niger ',
+		body: 'From here, the food will be distributed to the people who need it. (Sometimes the final destination is a warehouse, where the food will be stored until it’s needed, and other times the food is given out right away.)'
+	},
 ];
