35dae175cbb541ababfc7a29796aab7c0cdd6995
katevdub <kate@Kates-MacBook-Pro.local>
2013-11-15 09:25:19 -0500

    Working with counters and slice

---
+++ d3-test-map/data/process-text.js
@@ -0,0 +1,12 @@
+var process_text = [
+	{
+		headline: '1. Emergency Declared',
+		body: 'A food emergency can be declared in a few different ways. A country’s government can ask for help directly, or USAID’s FEWS system (Famine Early Warnings Systems Network) can detect the upcoming need for aid. Sometimes, the U.S. government has time to send a DART (Direct Action Response Team) to assess the needs of the country, but this doesn’t happen in every situation, due to time constraints. In the case of Niger, Prime Minister Mahamadou Danda appealed to the international community for emergency aid to stem the food insecurity crisis in March 2010. The aid the U.S. is providing to Niger is a longer term project, not a single emergency situation.'
+	},
+	{
+		headline: '2. USAID asks for NGO & shipping proposals - Washington, DC',
+		body: 'USAID requests proposals from aid agencies, like Catholic Relief Services, Save the Children or World Vision. The agencies propose how they would provide aid to the country, listing the specific food they’d like to send and how much. 
+'
+	}
+	
+];
