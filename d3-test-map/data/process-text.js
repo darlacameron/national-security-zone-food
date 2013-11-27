c30192f6f27187f9e02b700c31f5608686e531b3
katevdub <kate@Kates-MacBook-Pro.local>
2013-11-27 11:58:13 -0500

    Created index-kate/Added to gloabl styles/Added images folder
    So I wouldn't mess anything up. I thought floating the text right might
    work for our full-width website. I'm working on index-kennedy as an
    alternate layout with the text below. The styling is messy trying to
    get things aligned. Could probably use some help. :)

--- d3-test-map/data/process-text.js
+++ d3-test-map/data/process-text.js
@@ -1,42 +1,50 @@
 var process_text = [
 	{
-		headline: "1. Emergency Declared",
+		headline: "Emergency Declared",
 		body: "A food emergency can be declared in a few different ways. A country's government can ask for help directly, or USAID's FEWS system (Famine Early Warnings Systems Network) can detect the upcoming need for aid. Sometimes, the U.S. government has time to send a DART (Direct Action Response Team) to assess the needs of the country, but this doesn't happen in every situation, due to time constraints. In the case of Niger, Prime Minister Mahamadou Danda appealed to the international community for emergency aid to stem the food insecurity crisis in March 2010. The aid the U.S. is providing to Niger is a longer term project, not a single emergency situation.",
-		time: "-"
+		location: "Niger",
+		days: "--"
 	},
 	{
-		headline: "2. USAID asks for NGO & shipping proposals - Washington, DC",
+		headline: "USAID asks for NGO & shipping proposals",
 		body: "USAID requests proposals from aid agencies, like Catholic Relief Services, Save the Children or World Vision. The agencies propose how they would provide aid to the country, listing the specific food they'd like to send and how much.",
+		location: "Washington, DC",
 		days: "?"
 	},
 	{
-		headline: "3. Commodities are purchased - Kansas City",
+		headline: "Commodities are purchased",
 		body: "Once the government approves an aid agency's proposal, they sent the request for specific food items to the Kansas City Commodity Office, who sends out a call for bids to agribusiness. Agribusiness has two weeks to offer bids on how much they'd charge to provide specific products. Shipping companies also bid on how they'd transport the food.",
+		location: "Kansas City, Missouri",
 		days: "?"
 	},
 	{
-		headline: "4. Commodities are gathered and sent on their way - Wisconsin",
+		headline: "Commodities are gathered and sent on their way",
 		body: "The desired crops go from the farmers producing them to the mills where they'll be processed and, in some cases, bagged (sometimes they're shipped in bulk). Processing can include the addition of special vitamins and minerals to the product.",
+		location: "Cambria and Beaver Dam, Wisconsin (and others)",
 		days: "30"
 	},
 	{
-		headline: "5. Food arrives at its first stop - Chicago",
+		headline: "Food arrives at its first stop",
 		body: 'The crops travel by rail from the mill to "bridge facility," a step toward their final U.S. destination, their departure port.',
+		location: "Chicago, Illinois",
 		days: "44"
 	},
 	{
-		headline: "6. Food arrives at U.S. departure port - Norfolk, Virginia",
+		headline: "Food arrives at U.S. departure port",
 		body: "This is where the food is loaded onto a big ship to begin it's journey overseas. An inspector watches as it's loaded onto the ship, making sure bags aren't broken or otherwise sullied.",
+		location: "Norfolk, Virginia",
 		days: "50"
 	},
 	{
-		headline: "7. Food arrives at destination port - Cotonou, Benin",
+		headline: "Food arrives at destination port",
 		body: "The foreign port is where the NGO takes possession of the aid. If it was shipped in bulk (like corn filling up the hull of the ship), it's bagged here.",
+		location: "Cotonou, Benin",
 		days: "83"
 	},
 	{
-		headline: "8. Food gets to hungry people - Maradi, Niger ",
-		body: "From here, the food will be distributed to the people who need it. (Sometimes the final destination is a warehouse, where the food will be stored until it's needed, and other times the food is given out right away.)",
+		headline: "Food gets to hungry people",
+		body: "From here, the food will be distributed to the people who need it. Sometimes, like in the case of this shipment, the final destination is a warehouse where the food will be stored until it's needed. Other times the food is distributed right away.",
+		location: "Maradi, Niger",
 		days: "?"
 	},
 ];
