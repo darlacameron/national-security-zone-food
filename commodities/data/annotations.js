3241fef9c8b65715e438c51f4ae8f424a1379231
katevdub <kate@Kates-MacBook-Pro.local>
2013-11-30 21:55:43 -0500

    Commodities styling/Added annotations.js

---
+++ commodities/data/annotations.js
@@ -0,0 +1,32 @@
+var annotations = [
+	{
+		country: "Russia",
+		year: "1993",
+		text:"In 1993, the U.S. pledged $700 million in aid under the Food for Progress program, which sends commodities to be monetized (resold) for the promotion of agricultural development and trade.",
+		source:""
+	},
+	{
+		country: "Russia",
+		year: "1999",
+		text:"In 1999, the U.S. responded to a request for aid from Russia, which was facing a severe financial crisis and one of the worst grain harvests in history. This is one of the largest U.S. food aid programs to a single nation in history.",
+		source:""
+	},
+		{
+		country: "Ethiopia",
+		year: "2004",
+		text:"USAID has been assisting Ethiopia since its inception in 1961. Over the decades, aid goals have shifted from emergency drought relief to development programming throughout the region.",
+		source:""
+	},
+	{
+		country: "North Korea",
+		year: "2006",
+		text:"In 2006, the U.S. stopped providing food aid to North Korea in response to tightened restrictions on donor agencies and objections to North Korea’s nuclear programs.",
+		source:""
+	},
+	{
+		country: "Europe",
+		year: "2008",
+		text:"Following the dissolution of the Soviet Union in 1991, many post-Soviet states received aid as a means to promote democracy and stabilize the new countries. Of the 24 initial recipient countries, 11 have now graduated from U.S. assistance.",
+		source:""
+	},
+];
